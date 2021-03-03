e73f263926b217d816f3c2d031d5b013f9bec7e0
yijing <yijing5893@dingtalk.com>
2021-03-03 22:47:57 +0800

    个人中心页面

---
+++ components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<uni-status-bar fixed >
+			<block slot='left'></block>
+		</uni-status-bar>
+	</view>
+</template>
+
+<script>
+	import StatusBar from './uni-nav-bar/uni-status-bar.vue'
+	export default {
+		components:{
+			uni-status-bar:StatusBar
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
